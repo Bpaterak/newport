ea7247f32588e3df7874c0a1ba1785f0ef4b6b96
Brandon Paterak <bpaterak@gmail.com>
2013-08-19 19:46:22 -0400

    i guess added js

--- assets/js/dyn.js
+++ assets/js/dyn.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-	$("#content").load("design.html");
+$(document).ready(function () {
+     $("#content").load("test.html");
 });
-
-$('a').click(function(){
-	var page = $(this).attr('href');
-	$("#content").load(page);
-	
-	return false;
+$('a').click(function () {
+     var page = $(this).attr('href');
+     $("#content").load(page);
+	 
+    return false;
 });
